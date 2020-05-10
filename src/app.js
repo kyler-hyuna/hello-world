c9c4c74ba48a17e6bc1089553922e955c224a375
Kyle Roach <kyle.roach@hyuna.bb>
2020-05-09 21:18:39 -0400

    feat: Add eslint

--- src/app.js
+++ src/app.js
@@ -1,18 +1,18 @@
-require('dotenv').config()
+require('dotenv').config();
 
 const express = require('express');
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-require('./util/logger')(app)
+require('./util/logger')(app);
 
 app.get('/', (req, res) => {
   res.send({
-    message: `Hello ${process.env.NAME}`
+    message: `Hello ${process.env.NAME}`,
   });
 });
 
 app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`)
+  console.log(`Server started on port ${PORT}`);
 });
--- src/util/logger.js
+++ src/util/logger.js
@@ -1,9 +1,11 @@
-
-const fs = require('fs')
-const morgan = require('morgan')
-const path = require('path')
+const fs = require('fs');
+const morgan = require('morgan');
+const path = require('path');
 
 module.exports = (app) => {
-  const stream = fs.createWriteStream(path.join(__dirname, '../../__logs/access.log'), { flags: 'a' })
-  app.use(morgan('combined', { stream }))
-}
+  const stream = fs.createWriteStream(
+    path.join(__dirname, '../../__logs/access.log'),
+    { flags: 'a' }
+  );
+  app.use(morgan('combined', { stream }));
+};
